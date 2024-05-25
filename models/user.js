7efaac871e57eab0d7b43191553f9cd0b555f634
M Safwan Yasin <safwanyasin2002@gmail.com>
2024-05-25 15:47:40 +0300

    minor edits

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const createUser = async (user_id, nickname, email, hashedPassword) => {
 
 const findUserByEmail = async (email) => {
   const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-  return result.rows[0];
+  return result.rowCount === 0 ? false : result.rows[0];
 };
 
 const listUsers = async () => {
